5ea2fb687efffb23f2cc4018aa309fcb8aafd49a
Patrick Erdmann <pe@perdmann.de>
2022-08-06 16:07:04 +0200

    set port to 80

--- src/watering_webserver.py
+++ src/watering_webserver.py
@@ -211,5 +211,5 @@ if __name__ == "__main__":
     t = Thread(target=worker)
     t.start()
     
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=80)
         
